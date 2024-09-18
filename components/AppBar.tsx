13a89d7e07457eeacde147edc187a18a2aa66899
Monicah-Ngari <wakarimangari@gmail.com>
2024-09-18 16:01:04 +0300

    added stying to my appBar

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -4,16 +4,21 @@ import Constants from "expo-constants";
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: "grey",
+    backgroundColor: "#25292c",
+
+    paddingBottom: 5,
   },
-  // ...
+  text: {
+    fontSize: 25,
+    color: "white",
+  },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
       <TouchableOpacity>
-        <Text> Repositories</Text>
+        <Text style={styles.text}> Repositories</Text>
       </TouchableOpacity>
     </View>
   );
