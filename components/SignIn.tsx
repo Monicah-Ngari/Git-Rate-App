23bef349b01f0c78ea6d9c725376f9f2aec41c38
Monicah-Ngari <wakarimangari@gmail.com>
2024-09-23 05:24:45 +0300

    added a sign in form

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,7 +1,40 @@
-import { Text } from "react-native";
+import { Pressable, Text, TextInput, View } from "react-native";
+import { useFormik } from "formik";
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <>
+      <Text>The sign-in view</Text>
+      <View>
+        <TextInput
+          placeholder="Username"
+          value={formik.values.username}
+          onChangeText={formik.handleChange("username")}
+        />
+
+        <TextInput
+          placeholder="Password"
+          value={formik.values.password}
+          onChangeText={formik.handleChange("password")}
+          secureTextEntry={true}
+        />
+
+        <Pressable onSubmit={formik.handleSubmit}>
+          <Text>Sign In</Text>
+        </Pressable>
+      </View>
+    </>
+  );
 };
 
 export default SignIn;
