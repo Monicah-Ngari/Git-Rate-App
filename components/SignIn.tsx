1c791f55d45097d8e1a998eaf56228eba50083f3
Monicah-Ngari <wakarimangari@gmail.com>
2024-09-24 05:10:39 +0300

    implimemted yup in my signin form

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,20 +1,32 @@
 import { Pressable, Text, TextInput, View, StyleSheet } from "react-native";
 import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string().required("Username is required"),
+  password: Yup.string().required("Password is required"),
+});
 
 const styles = StyleSheet.create({
   text: {
     borderWidth: 1,
     borderColor: "black",
     padding: 8,
-    marginBottom: 12, // Adds space between the text inputs
+    marginBottom: 12,
   },
   button: {
     borderWidth: 1,
-    backgroundColor: "#0165D4", // Corrected hexadecimal value
+    backgroundColor: "#0165D4",
     borderColor: "black",
     padding: 8,
     color: "white",
-    textAlign: "center", // Centers the text inside the button
+    textAlign: "center",
+  },
+  textError: {
+    borderColor: "#d73a4a",
+  },
+  errorText: {
+    color: "#d73a4a",
   },
 });
 
@@ -31,22 +43,37 @@ const SignIn = () => {
 
   return (
     <>
-      {/* <Text>The sign-in view</Text> */}
       <View>
         <TextInput
           placeholder="Username"
           value={formik.values.username}
           onChangeText={formik.handleChange("username")}
-          style={styles.text}
+          onBlur={formik.handleBlur("username")}
+          style={
+            formik.touched.username && formik.errors.username
+              ? styles.textError
+              : styles.text
+          }
         />
+        {formik.touched.username && formik.errors.username ? (
+          <Text style={styles.errorText}>{formik.errors.username}</Text>
+        ) : null}
 
         <TextInput
           placeholder="Password"
           value={formik.values.password}
           onChangeText={formik.handleChange("password")}
+          onBlur={formik.handleBlur("password")}
           secureTextEntry={true}
-          style={styles.text}
+          style={
+            formik.touched.password && formik.errors.password
+              ? styles.textError
+              : styles.text
+          }
         />
+        {formik.touched.password && formik.errors.password ? (
+          <Text style={styles.errorText}>{formik.errors.password}</Text>
+        ) : null}
 
         <Pressable onPress={formik.handleSubmit}>
           <Text style={styles.button}>Sign In</Text>
